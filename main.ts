cc84015c05586f0002fb4e8bff0c2a91dc38509a
Niek-T <71765504+Niek-T@users.noreply.github.com>
2021-06-22 14:56:57 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,14 +1,8 @@
 let test = ""
 let reading = 0
 let temp = 0
-bluetooth.startAccelerometerService()
-bluetooth.startButtonService()
-bluetooth.startIOPinService()
-bluetooth.startLEDService()
-bluetooth.startTemperatureService()
-bluetooth.startMagnetometerService()
-led.setBrightness(64)
 let log: String[] = []
+led.setBrightness(64)
 basic.forever(function () {
     temp = input.temperature()
     pins.analogWritePin(AnalogPin.P0, 1023)
@@ -18,10 +12,9 @@ basic.forever(function () {
     reading,
     1023
     )
-    basic.showString("" + (input.temperature()))
     basic.showNumber(reading)
-    test = "temp:" + temp + " humditity:" + reading
-    if (log.length > 50) {
+    test = "temp:" + temp + " hum" + reading
+    if (log.length > 10) {
         log.push(test)
         log.slice(0, -1)
     } else {
